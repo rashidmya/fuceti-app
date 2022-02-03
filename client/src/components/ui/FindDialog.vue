d852d3e42c17a7df5ebb7e93baf2867dd9485b84
buga-sys <itsfreak69@gmail.com>
2022-02-03 13:07:32 +0400

    dialog component

---
+++ client/src/components/ui/FindDialog.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-dialog
+    v-model="prompt"
+    persistent
+    maximized
+    transition-show="slide-up"
+    transition-hide="slide-down"
+  >
+    <q-card class="bg-dark text-white">
+      <q-card-section>
+        <q-btn
+          flat
+          dense
+          icon="fas fa-chevron-down"
+          label="back"
+          color="white"
+          @click="emit('closeDialog')"
+        />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-input v-model="findUserInput" label="Standard" />
+        <button @click="addUser">Add</button>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRef, ref } from "vue";
+import { useQuasar } from "quasar";
+
+export default defineComponent({
+  props: ["showDialog"],
+  emits: ["closeDialog"],
+  setup(props, { emit }) {
+    const $q = useQuasar();
+    
+    const prompt = toRef(props, "showDialog");
+    const findUserInput = ref("");
+
+    function addUser() {
+      findUserInput.value = "";
+      emit("closeDialog");
+
+      $q.notify({
+        badgeClass: "alert-badge",
+        message: "Friend request has been sent!",
+        position: 'top'
+      });
+    }
+
+    return {
+      prompt,
+      findUserInput,
+      addUser,
+      emit,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.alert-badge {
+  color: pink;
+  background: white;
+  width: 100% !important;
+  height: 100% !important;
+}
+</style>
