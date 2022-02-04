f32518baf7f6b8f31095a4ebb85e862133a8ca3f
Rashid <itsfreak69@gmail.com>
2022-02-04 22:23:56 +0400

    chat show still not fixed scrolltobot

--- client/src/views/chats/show.vue
+++ client/src/views/chats/show.vue
@@ -1,36 +1,35 @@
 <template>
-  <div class="page-chat flex column">
+  <div class="q-pa-md row justify-center">
     <ChatHeader :userOnline="userOnline" />
-    <div class="q-pa-md row justify-center">
-      <div id="chat-container" class="full-width">
-        <q-chat-message
-          :text="['hey, how are you?']"
-          stamp="7 minutes ago"
-          sent
-        />
-        <q-chat-message
-          :text="[
-            'doing fine, how r you?',
-            'I just feel like typing a really, really, REALLY long message to annoy you...',
-          ]"
-          stamp="4 minutes ago"
-          text-color="black"
-          bg-color="pink-11"
-        />
-        <q-chat-message
-          :text="['Did it work?']"
-          stamp="1 minutes ago"
-          text-color="black"
-          bg-color="pink-11"
-        />
-        <q-chat-message
-          v-for="m in messages"
-          :key="m"
-          :text="m.text"
-          :stamp="moment(parseInt(m.stamp)).fromNow()"
-          :sent="m.sent"
-        />
-      </div>
+    <div id="chat-container" ref="chatContainer">
+      <q-chat-message
+        :text="['hey, how are you?']"
+        stamp="7 minutes ago"
+        sent
+      />
+      <q-chat-message
+        :text="[
+          'I just feel like typing a really, really, REALLY long message to annoy you...',
+        ]"
+        stamp="4 minutes ago"
+        text-color="black"
+        bg-color="blue-3"
+      />
+      <q-chat-message
+        v-for="x in 3"
+        :key="x"
+        :text="['Did it work?']"
+        stamp="1 minutes ago"
+        text-color="black"
+        bg-color="blue-3"
+      />
+      <q-chat-message
+        v-for="m in messages"
+        :key="m"
+        :text="m.text"
+        :stamp="moment(parseInt(m.stamp)).fromNow()"
+        :sent="m.sent"
+      />
     </div>
     <ChatFooter @newMessage="sendMessage" />
   </div>
@@ -56,6 +55,7 @@ export default defineComponent({
   setup() {
     const userOnline = ref(false);
     const messages = ref(new Array());
+    const chatContainer = ref<HTMLDivElement>();
 
     function sendMessage(newMessage: any) {
       const msg: Message = {
@@ -64,6 +64,9 @@ export default defineComponent({
         sent: true,
       };
       messages.value.push(msg);
+      if (chatContainer.value) {
+        chatContainer.value.scrollTop = chatContainer.value.scrollHeight;
+      }
     }
 
     return {
@@ -71,106 +74,18 @@ export default defineComponent({
       sendMessage,
       messages,
       moment,
+      chatContainer,
     };
   },
 });
 </script>
 
 <style scoped>
-.page-chat::after {
-  content: "";
-  display: block;
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  z-index: 0;
-  opacity: 0.01;
-  background-image: radial-gradient(
-      circle at 100% 150%,
-      silver 24%,
-      white 24%,
-      white 28%,
-      silver 28%,
-      silver 36%,
-      white 36%,
-      white 40%,
-      transparent 40%,
-      transparent
-    ),
-    radial-gradient(
-      circle at 0 150%,
-      silver 24%,
-      white 24%,
-      white 28%,
-      silver 28%,
-      silver 36%,
-      white 36%,
-      white 40%,
-      transparent 40%,
-      transparent
-    ),
-    radial-gradient(
-      circle at 50% 100%,
-      white 10%,
-      silver 10%,
-      silver 23%,
-      white 23%,
-      white 30%,
-      silver 30%,
-      silver 43%,
-      white 43%,
-      white 50%,
-      silver 50%,
-      silver 63%,
-      white 63%,
-      white 71%,
-      transparent 71%,
-      transparent
-    ),
-    radial-gradient(
-      circle at 100% 50%,
-      white 5%,
-      silver 5%,
-      silver 15%,
-      white 15%,
-      white 20%,
-      silver 20%,
-      silver 29%,
-      white 29%,
-      white 34%,
-      silver 34%,
-      silver 44%,
-      white 44%,
-      white 49%,
-      transparent 49%,
-      transparent
-    ),
-    radial-gradient(
-      circle at 0 50%,
-      white 5%,
-      silver 5%,
-      silver 15%,
-      white 15%,
-      white 20%,
-      silver 20%,
-      silver 29%,
-      white 29%,
-      white 34%,
-      silver 34%,
-      silver 44%,
-      white 44%,
-      white 49%,
-      transparent 49%,
-      transparent
-    );
-  background-size: 100px 50px;
+#chat-container {
+  height: 100%;
+  width: 100%;
 }
 .q-message {
   z-index: 1;
-}
-.message-form {
-  padding: 15px;
 }
 </style>
