9477cc9a00382474be2f12f9458deec87016eed8
Rashid <itsfreak69@gmail.com>
2022-02-08 13:02:15 +0400

    package updates

---
+++ client/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript'
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ['index','show','new']
+    }]
+  }
+}
