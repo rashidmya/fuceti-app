7c3caa07bdb6b297df161cca150822ffe51f6a1e
Rashid <itsfreak69@gmail.com>
2022-03-12 22:58:07 +0400

    dfjkl;hbsdfgjkl;hb

--- server/src/app.ts
+++ server/src/app.ts
@@ -145,6 +145,10 @@ io.on("connection", async (socket: UserSocket) => {
     socket.to(userId).emit('call hangup')
   })
 
+  socket.on('call answer', ({userId}) => {
+    socket.to(userId).emit('call answer')
+  })
+
   socket.on("disconnect", async () => {
     const matchingSockeets = await io.in(socket.userId!).allSockets();
     const isDisconnected = matchingSockeets.size === 0;
